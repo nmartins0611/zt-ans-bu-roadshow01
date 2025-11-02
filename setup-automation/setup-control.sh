5afc678f9132c98174648ca2787cffdd7268fe3b
Nuno Martins <nmartins@redhat.com>
2025-11-02 09:16:38 +0200

    Update setup-control.sh

--- setup-automation/setup-control.sh
+++ setup-automation/setup-control.sh
@@ -521,7 +521,7 @@ tee /tmp/setup.yml << EOF
       controller_host: "https://localhost"
       controller_username: admin
       controller_password: ansible123!
-      validate_certs: false
+      validate_certs: false2397
 
 EOF
 
@@ -534,4 +534,4 @@ EOF
 
 
 
-#ANSIBLE_COLLECTIONS_PATH=/tmp/ansible-automation-platform-containerized-setup-bundle-2.5-9-x86_64/collections/:/root/.ansible/collections/ansible_collections/ ansible-playbook -i /tmp/inventory /tmp/setup.yml
+ANSIBLE_COLLECTIONS_PATH=/tmp/ansible-automation-platform-containerized-setup-bundle-2.5-9-x86_64/collections/:/root/.ansible/collections/ansible_collections/ ansible-playbook -i /tmp/inventory /tmp/setup.yml
